87211a41fe25c113292cbc42dc87ae1922a6962d
John Carr <john.carr@isotoma.com>
2011-03-31 23:17:33 +0100

    Actually starts a VM and stops it

--- sidekick/test.py
+++ sidekick/test.py
@@ -1,5 +1,6 @@
 
 from sidekick.vm import vmware
+from ctypes import byref
 
 workstation = True
 
@@ -11,7 +12,7 @@ if workstation:
     password = ""
     vmpoweroptions = vmware.VIX_VMPOWEROP_LAUNCH_GUI
 
-    vmxpathinfo = "PATH_TO_VMX"
+    vmpathinfo = '/home/john/vmware/lucid/lucid.vmx'
 else:
     conntype = vmware.VIX_SERVICEPROVIDER_VMWARE_VI_SERVER
     hostname = "https://192.169.201.1:8333/sdk"
@@ -28,16 +29,16 @@ job = vmware.vix.VixHost_Connect(vmware.VIX_API_VERSION,
     conntype, hostname, hostport, username, password, options, vmware.VIX_INVALID_HANDLE, None, None)
 
 host = vmware.VixHandle()
-err = vmware.vix.VixJob_Wait(job, vmware.VIX_PROPERTY_JOB_RESULT_HANDLE, host, vmware.VIX_PROPERTY_NONE)
+err = vmware.vix.VixJob_Wait(job, vmware.VIX_PROPERTY_JOB_RESULT_HANDLE, byref(host), vmware.VIX_PROPERTY_NONE)
 if err != vmware.VIX_OK:
     raise ValueError(err)
 
 vmware.vix.Vix_ReleaseHandle(job)
 
 
-job = VixVM_Open(host, vmpathinfo, None, None)
+job = vmware.vix.VixVM_Open(host, vmpathinfo, None, None)
 vm = vmware.VixHandle()
-err = vmware.vix.VixJob_Wait(job, vmware.VIX_PROPERTY_JOB_RESULT_HANDLE, vm, vmware.VIX_PROPERTY_NONE)
+err = vmware.vix.VixJob_Wait(job, vmware.VIX_PROPERTY_JOB_RESULT_HANDLE, byref(vm), vmware.VIX_PROPERTY_NONE)
 if err != vmware.VIX_OK:
     raise ValueError(err)
 
@@ -49,7 +50,7 @@ err = vmware.vix.VixJob_Wait(job, vmware.VIX_PROPERTY_NONE)
 if err != vmware.VIX_OK:
     raise ValueError(err)
 
-vmware.vix.Vix_ReleaseHandle(jobHandle);
+vmware.vix.Vix_ReleaseHandle(job)
 
 
 job = vmware.vix.VixVM_PowerOff(vm, vmware.VIX_VMPOWEROP_NORMAL, None, None)
@@ -62,4 +63,6 @@ vmware.vix.Vix_ReleaseHandle(vm)
 
 vmware.vix.VixHost_Disconnect(host)
 
+def main():
+    pass
 
