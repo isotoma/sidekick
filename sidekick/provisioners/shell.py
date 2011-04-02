8c3fcf002d363aecceeaca47397e76410d6e14db
John Carr <john.carr@isotoma.com>
2011-04-02 23:27:26 +0100

    Use machine.run_script to make best use of vmware tools

--- sidekick/provisioners/shell.py
+++ sidekick/provisioners/shell.py
@@ -4,7 +4,5 @@ from sidekick.provisioner import Provisioner
 class ShellProvisioner(Provisioner):
 
     def provision(self):
-        self.machine.put(self.path. chmod=755, self.script)
-        self.machine.run(self.path)
-        self.machine.delete(self.path)
+        self.machine.run_script(self.script)
 
