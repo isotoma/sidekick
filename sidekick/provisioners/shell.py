1223e77255cee9c2e1da2b35835058892fc03988
John Carr <john.carr@isotoma.com>
2011-04-02 23:10:45 +0100

    Provision via a simple script

--- sidekick/provisioners/shell.py
+++ sidekick/provisioners/shell.py
@@ -4,6 +4,7 @@ from sidekick.provisioner import Provisioner
 class ShellProvisioner(Provisioner):
 
     def provision(self):
-        pass
-
+        self.machine.put(self.path. chmod=755, self.script)
+        self.machine.run(self.path)
+        self.machine.delete(self.path)
 
