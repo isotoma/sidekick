3eb67889a7c8fb7f22ec8730aaf374eadf632236
John Carr <john.carr@isotoma.com>
2011-04-03 13:30:39 +0100

    Mock out 'sidekick ssh'

---
+++ sidekick/commands/ssh.py
@@ -0,0 +1,28 @@
+
+from sidekick.command import ProjectCommand
+
+class Ssh(ProjectCommand):
+    name = "ssh"
+
+    def do(self):
+        #FIXME: Pick a VM:
+        #  - if there is more than 1 defined, must be specified
+        #  - if there is only 1 defined, ssh into that
+        #  - if there is only 1 defined and the name is specified it
+        #    must be right
+        vms = self.project.all_vms()
+        vm = vms[0]
+
+        if not vm.is_running():
+            raise errors.VmNotRunning()
+
+        command = ["ssh", "-A"]
+
+        # if deploy_user:
+        #     command.extend(['-l', deploy_user])
+
+        command.append(vm.get_ip())
+
+        os.spawnvp(os.P_OVERLAY, "ssh", command)
+
+
