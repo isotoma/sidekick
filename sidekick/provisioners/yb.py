b5a707a47aeedbb075d653805397d811ce21b4b3
John Carr <john.carr@isotoma.com>
2011-04-06 15:30:52 +0100

    Generate a suitable yay file out of Sidekick info

--- sidekick/provisioners/yb.py
+++ sidekick/provisioners/yb.py
@@ -1,6 +1,6 @@
-
+import StringIO
+from yay.config import Config, dump
 from yaybu.core.remote import RemoteRunner
-
 from sidekick.provisioner import Provisioner
 
 class YaybuProvisioner(Provisioner):
@@ -18,6 +18,23 @@ class YaybuProvisioner(Provisioner):
                 return True
 
     def provision(self):
+        conf = Config()
+
+        for recipe in self.machine.project.config.get("recipes", []):
+            config.load_uri(recipe)
+
+        for recipe in self.machine.config.get("recipes", [])
+            config.load_uri(recipe)
+
+        s = StringIO.StringIO(dump(self.machine.project.config.get("yaybu", {})))
+        config.load(s)
+
+        s = StringIO.StringIO(dump(self.machine.config.get("yaybu", {})))
+        config.load(s)
+
+        with open("foo.yay", "w") as f:
+            f.write(dump(config.get()))
+
         class opts:
             log_level = "info"
             logfile = "-"
