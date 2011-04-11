3c902a41d7df350c0074306697e60ff2bb00c4bd
John Carr <john.carr@isotoma.com>
2011-04-11 20:12:11 +0100

    Add a helper for working with a vbox session

--- sidekick/vm/virtualbox.py
+++ sidekick/vm/virtualbox.py
@@ -18,10 +18,12 @@ class Provider(object):
             os.environ["VBOX_PROGRAM_PATH"] = vbp
             sys.path.append(os.path.join(vbp, "sdk", "installer"))
 
+        from vboxapi import VirtualBoxManager
+
         self.globl = VirtualBoxManager(self.style, None)
         self.mgr = self.globl.mgr
         self.vb = self.globl.vb
-        self.ifaces = self.globl.constants
+        self.const = self.globl.constants
         self.remote = self.globl.remote
         self.type = self.globl.type
 
@@ -45,10 +47,39 @@ class WebProvider(object):
     style = None
 
 
+class Session(object):
+
+    def __init__(self, globl, machine):
+        self.globl = globl
+        self.mgr = self.globl.mgr
+        self.vb = self.globl.vb
+        self.const = self.globl.constants
+        self.remote = self.globl.remote
+        self.type = self.globl.type
+
+    def __enter__(self):
+        self.session = self.mgr.getSessionObject(self.vb)
+        self.vb.openExistingSession(session, self.machine.id)
+
+        if self.session.state != self.const.SessionState_Open:
+            self.session.close()
+            self.session = None
+            raise RuntimeError("Session to '%s' in wrong state: %s" % (self.macine.name, self.session.state))
+
+        return self.session
+
+    def __exit__(self, *args):
+        if self.session:
+            self.session.close()
+            self.session = None
+
+
 class VirtualMachine(object):
 
-    def __init__(self, host, path, default_powerop_start=low.VIX_VMPOWEROP_NORMAL):
-        pass
+    def __init__(self, provider, path, name):
+        self.provider = provider
+        self.path = path
+        self.name = name
 
     def get_ip(self):
         return ""
