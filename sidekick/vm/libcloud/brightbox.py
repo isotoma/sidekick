c5f5f4813bb717018aaa2b6b229d8d368963885c
John Carr <john.carr@isotoma.com>
2011-05-02 21:53:18 +0100

    Flesh out brightbox backend a bit

--- sidekick/vm/libcloud/brightbox.py
+++ sidekick/vm/libcloud/brightbox.py
@@ -6,6 +6,10 @@ from libcloud.providers import get_driver
 
 from sidekick.vm.base import BaseProvider, BaseMachine
 
+POWERSTATES = {
+    0: "running",
+    }
+
 
 class CloudProvider(BaseProvider):
 
@@ -24,16 +28,55 @@ class CloudMachine(BaseMachine):
     def __init__(self, driver, config):
         self.driver = driver
         self.config = config
+        self.node = None
 
-    def ex_wait_for_boot(self):
-        while True:
-            nodes = self.driver.list_nodes()
-            node = filter(lambda x: x.id == self.config['id'], nodes)[0]
+    def _refresh_node(self):
+        nodes = filter(lambda x: x.id == self.config['id'], self.driver.list_nodes())
+        self.node = nodes[0] if nodes else None
 
-            if node.state == 0:
-                break
-
+    def _wait_for_boot(self):
+        while self.get_powerstate() != "running":
             sleep(1)
 
+    def _assign_ip(self):
+        if self.node.public_ip:
+            return self.node.public_ip[0]
+
+        unmapped = [ip for ip in self.driver.ex_list_cloud_ips() if not ip['server']]
+        if unmapped:
+            ip = unmapped[0]
+        else:
+            ip = self.driver.ex_create_cloud_ip()
+
+        self.driver.ex_map_cloud_ip(ip['id'], self.node.extra['interfaces'][0]['id'])
+
+        return ip['public_ip']
+
+    def get_powerstate(self):
+        self._refresh_node()
+        return POWERSTATES[self.node.state]
+
     def power_on(self):
-        pass
+        self._refresh_node()
+        if self.node:
+            return
+
+        print "Finding size (nano)"
+        size = filter(lambda x: x.id == 'typ-4nssg', bb.list_sizes())[0]
+
+        print "Finding image (lucid 10.04 i686)"
+        image = filter(lambda x: x.id == 'img-hm6oj', bb.list_images())[0]
+
+        self.node = self.driver.create_node(name=self.config['name'], size=size, image=image)
+
+        self._wait_for_boot()
+        self._assign_ip()
+
+    def power_off(self):
+        self._refresh_node()
+
+        if not self.node:
+            return
+
+        self.driver.destroy_node(self.node)
+
